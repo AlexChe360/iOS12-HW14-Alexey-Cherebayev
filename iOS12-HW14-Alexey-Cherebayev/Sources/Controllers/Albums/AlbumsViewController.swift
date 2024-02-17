5e5f03f389aa005ed0abe85544150ade9cd15108
Alexey <devalex360@gmail.com>
2024-02-17 22:39:08 +0600

    Config project and add tabbar

---
+++ iOS12-HW14-Alexey-Cherebayev/Sources/Controllers/Albums/AlbumsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumsViewController.swift
+//  iOS12-HW14-Alexey-Cherebayev
+//
+//  Created by  Alexey on 17.02.2024.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
